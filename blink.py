d3ba715d074c6809c9411370720942bd1f9627ac
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2016-03-08 22:29:08 +0900

    Add exit handler

--- blink.py
+++ blink.py
@@ -3,8 +3,22 @@
 
 import mraa
 import time
+import signal
 
 print("Hello mraa\nVersion: %s" % mraa.getVersion())
+
+def exit_signal_handler(signo, frame):
+    gpio = mraa.Gpio(9)
+    gpio.write(0)
+    gpio.enable(False)
+    time.sleep(0.5)
+    print("exitting...")
+    exit()
+
+# register signal handlers
+signal.signal(signal.SIGHUP, exit_signal_handler)
+signal.signal(signal.SIGINT, exit_signal_handler)
+signal.signal(signal.SIGTERM, exit_signal_handler)
 
 gpio = mraa.Gpio(9)
 gpio.dir(mraa.DIR_OUT)
